fc1c54e45a9cacce7cafb610adf6996a3c53f2a3
Rodrigo-TL <rodrigodqbrn@gmail.com>
2026-07-10 21:46:43 -0300

    atividade 1 e 2  - atualização

--- src/atividades/atividade1/CalculadoraMediaEscolarRecuperação.java
+++ src/atividades/atividade1/CalculadoraMediaEscolarRecuperação.java
@@ -1,5 +1,18 @@
 package atividades.atividade1;
 
+import java.util.Scanner;
+
 public class CalculadoraMediaEscolarRecuperação {
+
+    public static void main (String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Qual sua média? ");
+
+        double media = entrada.nextDouble ();
+
+
+
+
 
 }
