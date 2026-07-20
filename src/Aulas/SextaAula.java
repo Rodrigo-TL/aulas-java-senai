8deaebfb41a0550eef5dfc4f3bfc8666da734387
Rodrigo-TL <rodrigodqbrn@gmail.com>
2026-07-20 20:36:36 -0300

    Aula 6 - atualização

--- src/Aulas/SextaAula.java
+++ src/Aulas/SextaAula.java
@@ -2,19 +2,24 @@ package Aulas;
 
 import Aulas.objeto.pessoa;
 
+import java.sql.SQLOutput;
+
 public class SextaAula {
     public static void main(String[] args) {
+        pessoa pessoa1 = new pessoa(
+                "João",
+                2004,
+                'M',
+                "Branco",
+                "Professor"
+        );
 
-        System.out.println ();
+        System.out.println(pessoa1.getNome());
 
+        System.out.println(pessoa1);
 
-        }
+        pessoa1.setNome("Fulano de tal");
 
-        public String nome;
-        public int anoNascimento;
-        public char sexualidade;
-        public String cor;
-
-
+        System.out.println(pessoa1);
     }
 }
--- src/Aulas/objeto/pessoa.java
+++ src/Aulas/objeto/pessoa.java
@@ -1,6 +1,40 @@
 package Aulas.objeto;
 
 public class pessoa {
+    private String nome;
+    private int anoNascimento;
+    private char genero;
+    private String cor;
+    private String profissao;
 
+    // Construtor que recebe todos os dados
+    public pessoa(String nome, int anoNascimento, char genero, String cor, String profissao) {
+        this.nome = nome;
+        this.anoNascimento = anoNascimento;
+        this.genero = genero;
+        this.cor = cor;
+        this.profissao = profissao;
+    }
 
+    // Método Getter para o Nome
+    public String getNome() {
+        return nome;
+    }
+
+    // Método Setter para o Nome
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    // Método para permitir imprimir o objeto na tela de forma organizada
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "nome='" + nome + '\'' +
+                ", anoNascimento=" + anoNascimento +
+                ", genero=" + genero +
+                ", cor='" + cor + '\'' +
+                ", profissao='" + profissao + '\'' +
+                '}';
+    }
 }
